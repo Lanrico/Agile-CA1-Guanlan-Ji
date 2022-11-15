fac1396eba86d064fb62d178bd6778c63068da37
季冠岚 <jgl228298909@gmail.com>
2022-11-15 13:09:25 +0000

    Delete the original lab tests

--- cypress/e2e/favourites.cy.js
+++ cypress/e2e/favourites.cy.js
@@ -17,31 +17,12 @@ describe("The favourites feature", () => {
     cy.visit("/");
   });
 
-  describe("Selecting favourites", () => {
-    it("selected movie card shows the red heart", () => {
-      cy.get(".MuiGrid-grid-md-4 .MuiCardHeader-root").eq(1).find("svg").should("not.exist");
-      cy.get("button[aria-label='add to favorites']").eq(1).click();
-      cy.get(".MuiGrid-grid-md-4 .MuiCardHeader-root").eq(1).find("svg");
-    });
-  });
-
   describe("The favourites page", () => {
     beforeEach(() => {
       // Select two favourites and navigate to Favourites page
       cy.get("button[aria-label='add to favorites']").eq(1).click();
       cy.get("button[aria-label='add to favorites']").eq(3).click();
       cy.get("button").contains("Favorites").click();
-    });
-    it("only the tagged movies are listed", () => {
-      cy.get(".MuiCardHeader-content").should("have.length", 2);
-      cy.get(".MuiCardHeader-content")
-        .eq(0)
-        .find("p")
-        .contains(movies[1].title);
-      cy.get(".MuiCardHeader-content")
-        .eq(1)
-        .find("p")
-        .contains(movies[3].title);
     });
 
     describe("delete the favourites", () => {
